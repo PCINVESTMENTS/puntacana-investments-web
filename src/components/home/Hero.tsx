e2a945f6fbe2754bc895c271874e93517ddb5765
Deploy User <deploy@example.com>
2026-01-14 22:57:39 -0400

    Lighten hero overlay for more impressive look

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -50,7 +50,7 @@ export default function Hero({ dict, featuredImages }: HeroProps) {
             </div>
 
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black/50 z-0 bg-gradient-to-b from-black/70 via-transparent to-primary-black/90"></div>
+            <div className="absolute inset-0 bg-black/20 z-0 bg-gradient-to-b from-black/40 via-transparent to-primary-black/60"></div>
 
             {/* Content */}
             <div className="relative z-10 text-center px-4 max-w-5xl mx-auto flex flex-col items-center">
