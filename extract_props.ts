4be099ff48e2993351127a88f51ce7e3a94fac09
Deploy User <uepcrealestate@gmail.com>
2026-03-09 22:54:21 -0400

    fix(build): remove extract_props.ts to unblock vercel Next.js build

--- extract_props.ts
+++
@@ -1,5 +0,0 @@
-import { properties } from './src/data/properties.ts';
-import fs from 'fs';
-
-fs.writeFileSync('properties_extracted.json', JSON.stringify(properties, null, 2));
-console.log(`Extracted ${properties.length} properties to JSON.`);
