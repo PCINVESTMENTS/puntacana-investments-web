120b349c6d0fae4e3db4c4b6f1de07594b66a01c
Ulises Ubiera <puntacanainvestments@MacBook-Air-de-Ulises.local>
2026-04-27 11:23:19 -0400

    feat(tracking): implement architecture for Google Tag Manager and Meta Pixel via env variables

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -93,12 +93,63 @@ export default async function RootLayout({
 }>) {
   const { lang } = await params;
   const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
   return (
     <html lang={lang} className="scroll-smooth">
+      <head>
+        {/* Google Tag Manager - Head */}
+        {gtmId && (
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
+            `}
+          </Script>
+        )}
+        
+        {/* Meta Pixel Code - Head */}
+        {metaPixelId && (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${metaPixelId}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
+      </head>
       <body
         className={`${inter.variable} ${playfair.variable} antialiased bg-primary-black text-white`}
       >
+        {/* Google Tag Manager - Body (noscript) */}
+        {gtmId && (
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+          </noscript>
+        )}
+
+        {/* Meta Pixel - Body (noscript) */}
+        {metaPixelId && (
+          <noscript>
+            <img height="1" width="1" style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        )}
+
         <CompareProvider>
           <LocalBusinessSchema lang={lang} />
           {children}
