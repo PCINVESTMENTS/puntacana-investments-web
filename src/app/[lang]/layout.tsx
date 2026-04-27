518700ca2afa38be52da27920971aad360495499
Ulises Ubiera <puntacanainvestments@MacBook-Air-de-Ulises.local>
2026-04-27 19:17:33 -0400

    perf: defer third-party scripts to lazyOnload for PageSpeed 100

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -102,21 +102,21 @@ export default async function RootLayout({
       <head>
         {/* Google Tag Manager - Head */}
         {gtmId && (
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`
+          <Script id="google-tag-manager" strategy="lazyOnload">
+            {\`
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
-            `}
+              })(window,document,'script','dataLayer','\${gtmId}');
+            \`}
           </Script>
         )}
         
         {/* Meta Pixel Code - Head */}
         {metaPixelId && (
-          <Script id="meta-pixel" strategy="afterInteractive">
-            {`
+          <Script id="meta-pixel" strategy="lazyOnload">
+            {\`
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -125,9 +125,9 @@ export default async function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${metaPixelId}');
+              fbq('init', '\${metaPixelId}');
               fbq('track', 'PageView');
-            `}
+            \`}
           </Script>
         )}
       </head>
@@ -176,8 +176,8 @@ export default async function RootLayout({
         {hubspotId && (
           <Script
             id="hs-script-loader"
-            strategy="afterInteractive"
-            src={`//js.hs-scripts.com/${hubspotId}.js`}
+            strategy="lazyOnload"
+            src={\`//js.hs-scripts.com/\${hubspotId}.js\`}
           />
         )}
       </body>
