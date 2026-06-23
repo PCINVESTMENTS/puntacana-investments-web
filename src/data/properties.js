574247092b470f55a2426bad37741e18f95fc6be
Ulises Ubiera <puntacanainvestments@MacBook-Air-de-Ulises.local>
2026-06-23 18:53:21 -0400

    fix: remove Rickroll from properties.js

--- src/data/properties.js
+++ src/data/properties.js
@@ -47,7 +47,7 @@ exports.properties = [
             "/images/the-beach-kitchen-island-view-punta-cana.jpg",
             "/images/the-beach-modern-living-room-interior-punta-cana.jpg"
         ],
-        videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ", // Placeholder
+        videoUrl: "",
         virtualTourUrl: "https://my.matterport.com/show/?m=aRGoTozjtCa",
         featured: false,
         preConstruction: true,
