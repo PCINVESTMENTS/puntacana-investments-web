b987102dcd07457893a75943f924b09dde37e536
Ulises Ubiera <puntacanainvestments@MacBook-Air-de-Ulises.local>
2026-07-11 22:10:14 -0400

    Perf: Install critters and enable optimizeCss to inline critical CSS and eliminate render blocking

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    optimizeCss: true,
     optimizePackageImports: ['react-icons'],
     serverActions: {
       bodySizeLimit: '10mb',
